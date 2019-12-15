ff5cc8a5321c67ebca3bb56da4c3d501b61c7730
unknown <rajnishsasnal@gmail.com>
2019-12-15 10:43:07 +0530

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+}
+
+//The JAR is build:
+//[INFO] Replacing main artifact F:\deploy-spring-boot-aws-eb-master\02-spring-boot-todo-rest-api-h2\target\02-todo-rest-api-h2-0.0.1-SNAPSHOT.jar
